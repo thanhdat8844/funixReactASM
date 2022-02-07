e3c5359e853b16b1a6da30815be12b7fa51413e4
Thanh Dat <dothanhdat275@gmail.com>
2022-02-07 23:55:17 +0900

    Validation Update- ASM3 Base Done

--- src/components/AddStaffComponent.js
+++ src/components/AddStaffComponent.js
@@ -9,6 +9,7 @@ import {
   ModalHeader,
   ModalBody,
   Button,
+  FormFeedback,
 } from "reactstrap";
 
 class AddStaff extends Component {
@@ -26,15 +27,70 @@ class AddStaff extends Component {
       overTime: 0,
       image: "/assets/images/alberto.png",
       staffs: this.props.staffs,
+      touched: {
+        name: false,
+        doB: false,
+        startDate: false,
+      },
     };
     this.toggleModal = this.toggleModal.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handBlur = this.handBlur.bind(this);
   }
+
   toggleModal() {
     this.setState({
       isModalOpen: !this.state.isModalOpen,
     });
+  }
+  handBlur = (field) => (evt) => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true },
+    });
+  };
+
+  validate(name, doB, startDate, salaryScale, annualLeave, overTime) {
+    const errors = {
+      name: "Yêu cầu nhập",
+      doB: "Yêu cầu nhập",
+      startDate: "Yêu cầu nhập",
+      salaryScale: "",
+      annualLeave: "",
+      overTime: "",
+    };
+    const today = new Date().getTime();
+    const birthday = new Date(doB).getTime();
+    const startday = new Date(startDate).getTime();
+    if (this.state.touched.name && name.length < 3) {
+      errors.name = "Yêu cầu nhập nhiều hơn 2 ký tự";
+    } else if (this.state.touched.name && name.length > 29) {
+      errors.name = "Yêu cầu nhập ít hơn 30 ký tự";
+    } else if (this.state.touched.name && name.length > 2 && name.length < 30) {
+      errors.name = "";
+    }
+    if (this.state.touched.doB && birthday > today) {
+      errors.doB = "Ngày sinh phải trước ngày hôm nay";
+    } else if (this.state.touched.doB && doB != "") {
+      errors.doB = "";
+    }
+    if (this.state.touched.startDate && startday > today) {
+      errors.startDate = "Ngày bắt đầu phải trước ngày hôm nay";
+    } else if (this.state.touched.startDate && startday < birthday) {
+      errors.startDate = "Ngày bắt đầu phải lớn hơn ngày sinh";
+    } else if (this.state.touched.doB && startDate != "") {
+      errors.startDate = "";
+    }
+    if (isNaN(salaryScale)) {
+      errors.salaryScale = "Hãy nhập bằng số";
+    }
+    if (isNaN(annualLeave)) {
+      errors.annualLeave = "Hãy nhập bằng số";
+    }
+    if (isNaN(overTime)) {
+      errors.overTime = "Hãy nhập bằng số";
+    }
+    return errors;
   }
   handleChange(event) {
     const target = event.target;
@@ -98,6 +154,14 @@ class AddStaff extends Component {
     }
   }
   render() {
+    const errors = this.validate(
+      this.state.name,
+      this.state.doB,
+      this.state.startDate,
+      this.state.salaryScale,
+      this.state.annualLeave,
+      this.state.overTime
+    );
     return (
       <div className="m-2">
         <Button
@@ -120,10 +184,14 @@ class AddStaff extends Component {
                   <Input
                     value={this.state.name}
                     onChange={this.handleChange}
+                    onBlur={this.handBlur("name")}
                     type="text"
                     id="name"
                     name="name"
+                    valid={errors.name === ""}
+                    invalid={errors.name !== ""}
                   ></Input>
+                  <FormFeedback>{errors.name}</FormFeedback>
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -134,11 +202,15 @@ class AddStaff extends Component {
                   <Input
                     value={this.state.doB}
                     onChange={this.handleChange}
+                    onBlur={this.handBlur("doB")}
                     type="date"
                     id="doB"
                     name="doB"
                     pattern="\d{2}-\d{2}-d{4}"
+                    valid={errors.doB === ""}
+                    invalid={errors.doB !== ""}
                   ></Input>
+                  <FormFeedback>{errors.doB}</FormFeedback>
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -149,11 +221,15 @@ class AddStaff extends Component {
                   <Input
                     value={this.state.startDate}
                     onChange={this.handleChange}
+                    onBlur={this.handBlur("startDate")}
                     type="date"
                     id="startDate"
                     name="startDate"
                     pattern="\d{2}-\d{2}-d{4}"
+                    valid={errors.startDate === ""}
+                    invalid={errors.startDate !== ""}
                   ></Input>
+                  <FormFeedback>{errors.startDate}</FormFeedback>
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -187,7 +263,11 @@ class AddStaff extends Component {
                     type="text"
                     id="salaryScale"
                     name="salaryScale"
+                    placeHolder="1.0 -> 3.0"
+                    valid={errors.salaryScale === ""}
+                    invalid={errors.salaryScale !== ""}
                   ></Input>
+                  <FormFeedback>{errors.salaryScale}</FormFeedback>
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -201,7 +281,10 @@ class AddStaff extends Component {
                     type="text"
                     id="annualLeave"
                     name="annualLeave"
+                    valid={errors.annualLeave === ""}
+                    invalid={errors.annualLeave !== ""}
                   ></Input>
+                  <FormFeedback>{errors.annualLeave}</FormFeedback>
                 </Col>
               </FormGroup>
               <FormGroup row>
@@ -215,12 +298,26 @@ class AddStaff extends Component {
                     type="text"
                     id="overTime"
                     name="overTime"
+                    valid={errors.overTime === ""}
+                    invalid={errors.overTime !== ""}
                   ></Input>
+                  <FormFeedback>{errors.annualLeave}</FormFeedback>
                 </Col>
               </FormGroup>
               <FormGroup row>
                 <Col md={{ size: 10, offset: 5 }}>
-                  <Button type="submit" color="primary">
+                  <Button
+                    type="submit"
+                    color="primary"
+                    disabled={
+                      !this.state.name ||
+                      !this.state.doB ||
+                      !this.state.startDate ||
+                      this.state.salaryScale === "" ||
+                      this.state.annualLeave === "" ||
+                      this.state.overTime === ""
+                    }
+                  >
                     Thêm
                   </Button>
                 </Col>
