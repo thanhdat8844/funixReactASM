9cdf8aec24c0760286e8a7bd6b5be3a223e3e4d4
Thanh Dat <dothanhdat275@gmail.com>
2022-02-09 00:30:26 +0900

    Redux Form base

--- src/components/AddStaffComponent.js
+++ src/components/AddStaffComponent.js
@@ -9,8 +9,9 @@ import {
   ModalHeader,
   ModalBody,
   Button,
-  FormFeedback,
+  Row,
 } from "reactstrap";
+import { Control, LocalForm, Erros } from "react-redux-form";
 
 class AddStaff extends Component {
   constructor(props) {
@@ -34,9 +35,7 @@ class AddStaff extends Component {
       },
     };
     this.toggleModal = this.toggleModal.bind(this);
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handBlur = this.handBlur.bind(this);
     this.handleClear = this.handleClear.bind(this);
   }
 
@@ -45,36 +44,22 @@ class AddStaff extends Component {
       isModalOpen: !this.state.isModalOpen,
     });
   }
-  handBlur = (field) => (evt) => {
-    this.setState({
-      touched: { ...this.state.touched, [field]: true },
-    });
-  };
-  handleChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-    this.setState({
-      [name]: value,
-    });
-  }
-  handleSubmit(event) {
+  handleSubmit(values) {
+    console.log(values);
     this.toggleModal();
-    event.preventDefault();
     const newStaff = {
       id: this.props.staffs.length,
-      name: this.state.name,
-      doB: this.state.doB,
-      salaryScale: this.state.salaryScale,
-      startDate: this.state.startDate,
-      department: this.depInfo(this.state.department),
-      annualLeave: this.state.annualLeave,
-      overTime: this.state.overTime,
-      image: this.state.image,
+      name: values.name,
+      doB: values.doB,
+      salaryScale: values.salaryScale,
+      startDate: values.startDate,
+      department: this.depInfo(values.department),
+      annualLeave: values.annualLeave,
+      overTime: values.overTime,
+      image: values.image,
     };
     this.state.staffs.push(newStaff);
     this.props.onClickAdd(newStaff);
-    console.log(this.state.staffs);
     this.handleClear();
   }
   handleClear() {
@@ -89,50 +74,6 @@ class AddStaff extends Component {
       overTime: 0,
     });
   }
-
-  validate(name, doB, startDate, salaryScale, annualLeave, overTime) {
-    const errors = {
-      name: "Yêu cầu nhập",
-      doB: "Yêu cầu nhập",
-      startDate: "Yêu cầu nhập",
-      salaryScale: "",
-      annualLeave: "",
-      overTime: "",
-    };
-    const today = new Date().getTime();
-    const birthday = new Date(doB).getTime();
-    const startday = new Date(startDate).getTime();
-    if (this.state.touched.name && name.length < 3) {
-      errors.name = "Yêu cầu nhập nhiều hơn 2 ký tự";
-    } else if (this.state.touched.name && name.length > 29) {
-      errors.name = "Yêu cầu nhập ít hơn 30 ký tự";
-    } else if (this.state.touched.name && name.length > 2 && name.length < 30) {
-      errors.name = "";
-    }
-    if (this.state.touched.doB && birthday > today) {
-      errors.doB = "Ngày sinh phải trước ngày hôm nay";
-    } else if (this.state.touched.doB && doB != "") {
-      errors.doB = "";
-    }
-    if (this.state.touched.startDate && startday > today) {
-      errors.startDate = "Ngày bắt đầu phải trước ngày hôm nay";
-    } else if (this.state.touched.startDate && startday < birthday) {
-      errors.startDate = "Ngày bắt đầu phải lớn hơn ngày sinh";
-    } else if (this.state.touched.doB && startDate != "") {
-      errors.startDate = "";
-    }
-    if (isNaN(salaryScale)) {
-      errors.salaryScale = "Hãy nhập bằng số";
-    }
-    if (isNaN(annualLeave)) {
-      errors.annualLeave = "Hãy nhập bằng số";
-    }
-    if (isNaN(overTime)) {
-      errors.overTime = "Hãy nhập bằng số";
-    }
-    return errors;
-  }
-
   depInfo(dep) {
     switch (dep) {
       default:
@@ -169,14 +110,6 @@ class AddStaff extends Component {
     }
   }
   render() {
-    const errors = this.validate(
-      this.state.name,
-      this.state.doB,
-      this.state.startDate,
-      this.state.salaryScale,
-      this.state.annualLeave,
-      this.state.overTime
-    );
     return (
       <div className="m-2">
         <Button
@@ -190,154 +123,117 @@ class AddStaff extends Component {
         <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
           <ModalHeader toggle={this.toggleModal}>Thêm nhân viên</ModalHeader>
           <ModalBody>
-            <Form onSubmit={this.handleSubmit}>
-              <FormGroup row>
+            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+              <Row className="form-group">
                 <Label htmlFor="name" md={4}>
                   Tên
                 </Label>
                 <Col md={8}>
-                  <Input
-                    value={this.state.name}
-                    onChange={this.handleChange}
-                    onBlur={this.handBlur("name")}
-                    type="text"
+                  <Control.text
+                    model=".name"
                     id="name"
                     name="name"
-                    valid={errors.name === ""}
-                    invalid={errors.name !== ""}
-                  ></Input>
-                  <FormFeedback>{errors.name}</FormFeedback>
+                    className="form-control"
+                  />
                 </Col>
-              </FormGroup>
-              <FormGroup row>
+              </Row>
+              <Row className="form-group">
                 <Label htmlFor="doB" md={4}>
                   Ngày sinh
                 </Label>
                 <Col md={8}>
-                  <Input
-                    value={this.state.doB}
-                    onChange={this.handleChange}
-                    onBlur={this.handBlur("doB")}
+                  <Control
                     type="date"
+                    model=".doB"
                     id="doB"
                     name="doB"
                     pattern="\d{2}-\d{2}-d{4}"
-                    valid={errors.doB === ""}
-                    invalid={errors.doB !== ""}
-                  ></Input>
-                  <FormFeedback>{errors.doB}</FormFeedback>
+                    className="form-control"
+                  />
                 </Col>
-              </FormGroup>
-              <FormGroup row>
+              </Row>
+              <Row className="form-group">
                 <Label htmlFor="startDate" md={4}>
                   Ngày vào công ty
                 </Label>
                 <Col md={8}>
-                  <Input
-                    value={this.state.startDate}
-                    onChange={this.handleChange}
-                    onBlur={this.handBlur("startDate")}
+                  <Control
+                    model=".startDate"
                     type="date"
                     id="startDate"
                     name="startDate"
                     pattern="\d{2}-\d{2}-d{4}"
-                    valid={errors.startDate === ""}
-                    invalid={errors.startDate !== ""}
-                  ></Input>
-                  <FormFeedback>{errors.startDate}</FormFeedback>
+                    className="form-control"
+                  />
                 </Col>
-              </FormGroup>
-              <FormGroup row>
+              </Row>
+              <Row className="form-group">
                 <Label htmlFor="department" md={4}>
                   Phòng ban
                 </Label>
                 <Col md={8}>
-                  <Input
-                    value={this.state.department}
-                    onChange={this.handleChange}
-                    type="select"
+                  <Control.select
+                    model=".department"
                     id="department"
                     name="department"
+                    defaultValue={this.state.department}
                   >
                     <option>Sale</option>
                     <option>HR</option>
                     <option>Marketing</option>
                     <option>IT</option>
                     <option>Finance</option>
-                  </Input>
+                  </Control.select>
                 </Col>
-              </FormGroup>
-              <FormGroup row>
+              </Row>
+              <Row className="form-group">
                 <Label htmlFor="salaryScale" md={4}>
                   Hệ số lương
                 </Label>
                 <Col md={8}>
-                  <Input
-                    value={this.state.salaryScale}
-                    onChange={this.handleChange}
-                    type="text"
+                  <Control.text
+                    model=".salaryScale"
                     id="salaryScale"
                     name="salaryScale"
+                    defaultValue={this.state.salaryScale}
                     placeHolder="1.0 -> 3.0"
-                    valid={errors.salaryScale === ""}
-                    invalid={errors.salaryScale !== ""}
-                  ></Input>
-                  <FormFeedback>{errors.salaryScale}</FormFeedback>
+                  />
                 </Col>
-              </FormGroup>
-              <FormGroup row>
+              </Row>
+              <Row className="form-group">
                 <Label htmlFor="annualLeave" md={4}>
                   Số ngày nghỉ còn lại
                 </Label>
                 <Col md={8}>
-                  <Input
-                    value={this.state.annualLeave}
-                    onChange={this.handleChange}
-                    type="text"
+                  <Control.text
+                    model=".annualLeave"
                     id="annualLeave"
                     name="annualLeave"
-                    valid={errors.annualLeave === ""}
-                    invalid={errors.annualLeave !== ""}
-                  ></Input>
-                  <FormFeedback>{errors.annualLeave}</FormFeedback>
+                    defaultValue={this.state.annualLeave}
+                  />
                 </Col>
-              </FormGroup>
-              <FormGroup row>
+              </Row>
+              <Row className="form-group">
                 <Label htmlFor="overTime" md={4}>
                   Số giờ đã làm thêm
                 </Label>
                 <Col md={8}>
-                  <Input
-                    value={this.state.overTime}
-                    onChange={this.handleChange}
-                    type="text"
+                  <Control.text
+                    model=".overTime"
                     id="overTime"
                     name="overTime"
-                    valid={errors.overTime === ""}
-                    invalid={errors.overTime !== ""}
-                  ></Input>
-                  <FormFeedback>{errors.annualLeave}</FormFeedback>
+                    defaultValue={this.state.overTime}
+                  />
                 </Col>
-              </FormGroup>
+              </Row>
               <FormGroup row>
                 <Col md={{ size: 10, offset: 5 }}>
-                  <Button
-                    type="submit"
-                    color="primary"
-                    disabled={
-                      !this.state.name ||
-                      !this.state.doB ||
-                      !this.state.startDate ||
-                      this.state.salaryScale === "" ||
-                      this.state.annualLeave === "" ||
-                      this.state.overTime === ""
-                    }
-                  >
+                  <Button type="submit" value="submit" color="primary">
                     Thêm
                   </Button>
                 </Col>
               </FormGroup>
-            </Form>
+            </LocalForm>
           </ModalBody>
         </Modal>
       </div>
